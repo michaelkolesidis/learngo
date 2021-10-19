f568d0e3c04aae081b6a2d8366d7e71e4ced8177
Michael Kolesidis <michael.kolesidis@gmail.com>
2021-10-19 14:16:16 +0300

    Update Hello.go

--- Hello.go
+++ Hello.go
@@ -1,7 +1,13 @@
+/* The package “main” tells the Go compiler that the package should compile as an executable program 
+ * instead of a shared library. */
 package main
 
+/* Package fmt implements formatted I/O with functions analogous to C's printf and scanf */
 import "fmt"
 
+/* The main function in the package “main” will be the entry point of our executable program. */
 func main() {
+	/* he fmt.Println() function in Go language formats using the default formats for its operands 
+	 * and writes to standard output. */
 	fmt.Println("Hello, world.")
 }
